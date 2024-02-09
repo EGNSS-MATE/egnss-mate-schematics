9f9e47d99e41e275824811e7d2e9b6ef80958928
rehrler <45822498+rehrler@users.noreply.github.com>
2024-02-09 15:30:21 +0100

    test pygithub

--- sql/dm_S_DM_TRACK.sql
+++ sql/dm_S_DM_TRACK.sql
@@ -10,3 +10,4 @@ CREATE TABLE IF NOT EXISTS TABLE S_DM_TRACK (
 	CREATED_TIMESTAMP TIMESTAMP_NTZ(6) COMMENT 'Timestamp of the row creation'
 )
 COMMENT='Standardization table for digital map track'
+;
