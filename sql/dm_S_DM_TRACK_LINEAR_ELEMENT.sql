e0d3071cff86d98befbbdcf99cb388829b1a5ea4
rehrler <45822498+rehrler@users.noreply.github.com>
2024-02-09 15:40:50 +0100

    test pygithub

--- sql/dm_S_DM_TRACK_LINEAR_ELEMENT.sql
+++ sql/dm_S_DM_TRACK_LINEAR_ELEMENT.sql
@@ -11,3 +11,4 @@ CREATE TABLE IF NOT EXISTS TABLE S_DM_TRACK_LINEAR_ELEMENT (
 	CREATED_TIMESTAMP TIMESTAMP_NTZ(6) COMMENT 'Timestamp of the row creation'
 )
 COMMENT='Standardization table for digital map track as geometry object'
+;
