4f4413afa3fed44ef53e0fbabe0b6dc480ae298d
rehrler <45822498+rehrler@users.noreply.github.com>
2024-02-09 15:28:36 +0100

    test pygithub

--- sql/admin_S_COORDINATE_SYSTEM.sql
+++ sql/admin_S_COORDINATE_SYSTEM.sql
@@ -1,8 +1,7 @@
-CREATE TABLE IF NOT EXISTS S_COORDINATE_SYSTEM
-(
-    COORD_SYSTEM_NAME STRING COMMENT 'Name of the coordinate system',
-    EPSG              INTEGER COMMENT 'EPSG code of the coordinate system according https://epsg.io/',
-    VALID_FROM        TIMESTAMP_NTZ(6) COMMENT 'UTC date valid from',
-    VALID_TO          TIMESTAMP_NTZ(6) COMMENT 'UTC date valid to'
+CREATE TABLE IF NOT EXISTS TABLE S_COORDINATE_SYSTEM (
+	COORD_SYSTEM_NAME VARCHAR(16777216) COMMENT 'Name of the coordinate system',
+	EPSG NUMBER(38,0) COMMENT 'EPSG code of the coordinate system according https://epsg.io/',
+	VALID_FROM TIMESTAMP_NTZ(6) COMMENT 'UTC date valid from',
+	VALID_TO TIMESTAMP_NTZ(6) COMMENT 'UTC date alid to'
 )
-    COMMENT = 'Definitions of the coordinate systems'
+COMMENT='Definitions of the coordinate systems'
